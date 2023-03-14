044dd399cca704dac47d772a218f77fa2f826e32
djsantos <djsantos@hcpa.edu.br>
2023-03-14 10:07:41 -0300

    Código inicial

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="my-app">
+      <header className="my-app-header">
+        <img src={logo} className="header-logo" alt="logo" />
+        <h1>Hello from React Js!</h1>
+      </header>
+
+
+
+    </div>
+  );
+}
+
+export default App;
